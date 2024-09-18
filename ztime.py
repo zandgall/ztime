89363b4a0000b10b88ecba9de524264860a372ce
Zander Gall <alexanderdgall@gmail.com>
2024-09-18 18:50:00 -0500

    Scheduling and Printing

--- ztime.py
+++ ztime.py
@@ -13,6 +13,9 @@ from PIL import Image, ImageDraw, ImageFont
 from colorama import Fore, Style
 import colorsys
 
+terminal_rows, terminal_columns = os.popen('stty size', 'r').read().split()
+terminal_rows = int(terminal_rows)
+terminal_columns = int(terminal_columns)
 def unix_time(string):
 	return int(time.mktime(parse(string).timetuple()))
 
@@ -20,15 +23,18 @@ def sunix_time(string):
 	return str(unix_time(string))
 
 log_string = ""
-def log(*strings):
+def log(*strings, end="\n"):
 	global log_string
-	print(*strings)
+	print(*strings, end=end)
 	for string in strings:
 		log_string += string.replace(Fore.BLUE, "").replace(Fore.BLACK, "").replace(Fore.CYAN, "").replace(Fore.GREEN, "").replace(Fore.MAGENTA, "").replace(Fore.RED, "").replace(Fore.YELLOW, "").replace(Fore.WHITE, "").replace(Style.BRIGHT, "").replace(Style.DIM, "").replace(Style.NORMAL, "").replace(Style.RESET_ALL, "") + " "
-	log_string += "\n"
+	log_string += end
 
 command_palette = {
 	"draw": f"{Fore.BLUE}begin {Style.DIM}end {Fore.WHITE}width {Fore.GREEN}filters...{Style.RESET_ALL}",
+	"print": f"{Fore.BLUE}begin {Style.DIM}end {Fore.WHITE}height {Fore.GREEN}filters...{Style.RESET_ALL}",
+	"schedule": f"{Fore.GREEN}type {Fore.BLUE}begin {Style.DIM}end{Style.RESET_ALL}".ljust(32+3*5)+f" {Style.DIM}alias: add{Style.RESET_ALL}",
+	"resolve": "",
 	"enter": f"{Fore.GREEN}type {Fore.BLUE}begin {Style.DIM}end{Style.RESET_ALL}".ljust(32+3*5)+f" {Style.DIM}alias: add{Style.RESET_ALL}",
 	"type": f"{Fore.GREEN}type {Fore.RED}color{Style.RESET_ALL}",
 	"remove": f"{Fore.YELLOW}id{Style.RESET_ALL}",
@@ -44,7 +50,10 @@ def instructions():
 	log("Time taking tool. ")
 	log("\nPossible commands:")
 	log("draw".rjust(10), "|", command_palette["draw"])
+	log("print".rjust(10), "|", command_palette["print"])
 	log("enter".rjust(10), "|", command_palette["enter"])
+	log("schedule".rjust(10), "|", command_palette["enter"])
+	log("resolve".rjust(10), "| ")
 	log("type".rjust(10), "|", command_palette["type"])
 	log("remove".rjust(10), "|", command_palette["remove"])
 	log("modify".rjust(10), "|", command_palette["modify"])
@@ -64,7 +73,7 @@ def ztime_main(argv, doc_path):
 	# Create time.xml document if it doesn't exist
 	if not os.path.isfile(doc_path):
 		log(f"Couldn't find \"{doc_path}\", creating file.")
-		tree = ET.fromstring("<data><types></types><entries></entries><current></current></data>")
+		tree = ET.ElementTree(ET.fromstring("<data><types></types><entries></entries><current></current><schedule></schedule></data>"))
 		tree.write(doc_path)
 
 	if argv[1] == "draw":
@@ -72,6 +81,23 @@ def ztime_main(argv, doc_path):
 			log("Usage: draw |", command_palette["draw"])
 			return log_string
 		draw(argv, doc_path)
+	elif argv[1] == "print":
+		if len(argv) < 3:
+			log("Usage: print |", command_pallete["print"])
+			return log_string
+		ztime_print(argv, doc_path)
+	elif argv[1] == "schedule":
+		if len(argv) < 4:
+			log("Usage: schedule |", command_palette["schedule"])
+			return log_string
+		
+		tree = ET.parse(doc_path)
+		schedule = tree.getroot().find("schedule")
+		end = sunix_time(argv[4]) if len(argv) > 4 else str(int(time.mktime(dt.datetime.now().timetuple())))
+		entry = ET.SubElement(schedule, argv[2])
+		entry.attrib.update(begin=sunix_time(argv[3]), end=end)
+		tree.write(doc_path)
+		log("Scheduled", argv[2])
 	elif argv[1] == "enter" or argv[1] == "add":
 		if len(argv) < 4:
 			log("Usage: enter |", command_palette["enter"])
@@ -128,10 +154,10 @@ def ztime_main(argv, doc_path):
 			log("(", entry.attrib.get("id"), ") type is now", argv[4])
 		if argv[3] == 'b':
 			entry.attrib.update(begin=sunix_time(argv[4]))
-			log("(", entry.attrib.get("id"), ") begin is now is now", dt.datetime.fromtimestamp(int(entry.attrib.get("begin"))).strftime("%m/%d/%y %I:%M %p"))
+			log("(", entry.attrib.get("id"), ") begin is now is now", dt.datetime.fromtimestamp(int(entry.attrib.get("begin"))).strftime("%m/%d/%y %H:%M"))
 		if argv[3] == 'e':
 			entry.attrib.update(end=sunix_time(argv[4]))
-			log("(", entry.attrib.get("id"), ") end is now is now", dt.datetime.fromtimestamp(int(entry.attrib.get("end"))).strftime("%m/%d/%y %I:%M %p"))
+			log("(", entry.attrib.get("id"), ") end is now is now", dt.datetime.fromtimestamp(int(entry.attrib.get("end"))).strftime("%m/%d/%y %H:%M"))
 		tree.write(doc_path)
 		log("Modified", entry.tag, "'s", "(", entry.attrib.get("id"), ")")
 	elif argv[1] == "list":
@@ -157,9 +183,9 @@ def ztime_main(argv, doc_path):
 			if id >= low and id < high and ((entry.tag == argv[4]) if len(argv) > 4 else True):
 				output += entry.tag.ljust(tag_width)
 				output += (" ({:"+str(id_width)+"d}) ").format(id)
-				output += dt.datetime.fromtimestamp(int(entry.attrib.get("begin"))).strftime("%m/%d/%y %I:%M %p")
+				output += dt.datetime.fromtimestamp(int(entry.attrib.get("begin"))).strftime("%m/%d/%y %H:%M")
 				output += " - "
-				output += dt.datetime.fromtimestamp(int(entry.attrib.get("end"))).strftime("%m/%d/%y %I:%M %p")
+				output += dt.datetime.fromtimestamp(int(entry.attrib.get("end"))).strftime("%m/%d/%y %H:%M")
 				output += "\n"
 		log(output)
 	elif argv[1] == "start" or argv[1] == "begin":
@@ -176,6 +202,41 @@ def ztime_main(argv, doc_path):
 		ET.SubElement(current, type, {"begin": sunix_time(argv[3]) if len(argv) > 3 else str(int(time.mktime(dt.datetime.now().timetuple())))})
 		tree.write(doc_path)
 		log("Started", type)
+	elif argv[1] == "resolve":
+		log("Hello!")
+		tree = ET.parse(doc_path)
+		schedule = tree.getroot().find("schedule")
+		entries = tree.getroot().find("entries")
+		for scheduled in schedule:
+			log("Is \"", scheduled.tag, "\" (", dt.datetime.fromtimestamp(int(scheduled.attrib.get("begin"))).strftime("%m/%d/%y %H:%M"), " - ", dt.datetime.fromtimestamp(int(scheduled.attrib.get("end"))).strftime("%m/%d/%y %H:%M"), ") ready? Y/N: ")
+			resp = input()
+			if resp=="N" or resp=="n":
+				continue
+			log("(Blank for unchanged)")
+			log("Type: ")
+			tag = input()
+			if tag=="":
+				tag = scheduled.tag
+			log("Begin time: ")
+			begin = input()
+			if begin=="":
+				begin = scheduled.attrib.get("begin")
+			else:
+				begin = sunix_time(begin)
+			log("End time: ")
+			end = input()
+			if end=="":
+				end = scheduled.attrib.get("end")
+			else:
+				end = sunix_time(end)
+			
+			entry = ET.SubElement(entries, tag)
+			entry.attrib.update(id=entries.attrib.get("next_id"), begin=begin, end=end)
+			entries.attrib.update(next_id=str(int(entries.attrib.get("next_id"))+1))
+			schedule.remove(scheduled)
+			log("Entered", tag, "(", entry.attrib.get("id"), ")")
+		tree.write(doc_path)
+		return log_string
 	elif argv[1] == "stop" or argv[1] == "end":
 		if len(argv) < 3:
 			log("Usage: stop |", command_palette["stop"])
@@ -184,9 +245,9 @@ def ztime_main(argv, doc_path):
 		type = argv[2]
 		tree = ET.parse(doc_path)
 		current = tree.getroot().find("current")
+		entries = tree.getroot().find("entries")
 		for running in current:
 			if running.tag == type:
-				entries = tree.getroot().find("entries")
 
 				entry = ET.SubElement(entries, type)
 				entry.attrib.update(id=entries.attrib.get("next_id"), begin=running.attrib["begin"], end=sunix_time(argv[3]) if len(argv) > 3 else str(int(time.mktime(dt.datetime.now().timetuple()))))
@@ -216,7 +277,7 @@ def ztime_main(argv, doc_path):
 			if len(run.tag) > max_len:
 				max_len = len(run.tag)
 		for run in running:
-			log(run.tag.rjust(max_len+2), "|", dt.datetime.fromtimestamp(int(run.attrib.get("begin"))).strftime("%m/%d/%y %I:%M %p"))
+			log(run.tag.rjust(max_len+2), "|", dt.datetime.fromtimestamp(int(run.attrib.get("begin"))).strftime("%m/%d/%y %H:%M"))
 	elif argv[1] == "stat":
 		if len(argv) < 4:
 			log("Usage: stat |", command_palette["stat"])
@@ -267,9 +328,90 @@ def stat(argv, doc_path):
 	if end-begin > 86400*14:
 		log(time_length_string(time_spent / ((end-begin)/604800)), "per week")
 
+def ztime_print(argv, doc_path):
+	tree = ET.parse(doc_path)
+	entries_t = tree.getroot().find("entries")
+	current_t = tree.getroot().find("current")
+	entries = []
+	for e in entries_t:
+		entries.append(e)
+	for e in current_t:
+		entries.append(e)
+	included_entries = []
+
+	low = unix_time(argv[2])
+	high = unix_time(argv[3]) if len(argv) > 3 else int(time.mktime(dt.datetime.now().timetuple()))
+
+	widthProvided = len(argv) > 4 and argv[4].isnumeric()
+	width = int(argv[4]) if widthProvided else terminal_columns
+	height = 1
+
+	filterStart = 5 if widthProvided else 4
+	filter = len(argv) > filterStart
+
+	for entry in entries:
+		matchesFilter = not filter
+		if filter:
+			for f in range(filterStart, len(argv)):
+				if argv[f] == entry.tag:
+					matchesFilter = True
+		if not "end" in entry.attrib:
+			entry.attrib["end"] = "10000000000"
+		if intersect(low, high, entry.attrib["begin"], entry.attrib["end"]) and matchesFilter:
+			level = 0
+			i = 0
+			while i < len(included_entries):
+				included_entry = included_entries[i]
+				if intersect(entry.attrib["begin"], entry.attrib["end"], included_entry["xml"].attrib["begin"], included_entry["xml"].attrib["end"]) and included_entry["level"] == level:
+					level += 1
+					i = 0
+				else:
+					i += 1
+			if height < level + 1:
+				height = level + 1
+			included_entries.append({"xml": entry, "begin": int(entry.attrib["begin"]), "end": int(entry.attrib["end"]), "level": level, "i": 0})
+
+	palette = {}
+	types = tree.getroot().find("types")
+	for type in types:
+		palette.update({type.tag: type.attrib["color"]})
+
+	for y in range(height):
+		for x in range(width):
+			t = (x / width) * (high - low) + low
+			hit = False
+			for include in included_entries:
+				if include["level"] == y and t >= include["begin"] and t < include["end"]:
+					color_str = palette[include["xml"].tag] if include["xml"].tag in palette else "#ffffff"
+					rb = int(color_str[1:3], 16)
+					gb = int(color_str[3:5], 16)
+					bb = int(color_str[5:7], 16)
+					(h, s, v) = colorsys.rgb_to_hsv(rb / 255.0, gb/255.0, bb/255.0)
+					h += 0.25
+					if h > 1:
+						h -= 1
+					s = 1 - s
+					v = 1 - v
+					(rf, gf, bf) = colorsys.hsv_to_rgb(h, s, v)
+					if include["i"] < len(include["xml"].tag):
+						log(f"\033[38;2;{int(255*rf)};{int(255*gf)};{int(255*bf)}m\033[48;2;{rb};{gb};{bb}m{include["xml"].tag[include["i"]]}{Style.RESET_ALL}", end="")
+						include["i"] = include["i"] + 1
+					else:
+						log('\033[48;2;'+str(rb)+';'+str(gb)+';'+str(bb)+'m '+Style.RESET_ALL, end="")
+					hit = True
+			if hit == False:
+				log(' ', end="")
+		log("")
+
 def draw(argv, doc_path):
 	tree = ET.parse(doc_path)
-	entries = tree.getroot().find("entries")
+	entries_t = tree.getroot().find("entries")
+	current_t = tree.getroot().find("current")
+	entries = []
+	for e in entries_t:
+		entries.append(e)
+	for e in current_t:
+		entries.append(e)
 	included_entries = []
 
 	low = unix_time(argv[2])
@@ -291,6 +433,8 @@ def draw(argv, doc_path):
 			for f in range(filterStart, len(argv)):
 				if argv[f] == entry.tag:
 					matchesFilter = True
+		if not "end" in entry.attrib:
+			entry.attrib["end"] = "10000000000"
 		if intersect(low, high, entry.attrib["begin"], entry.attrib["end"]) and matchesFilter:
 			level = 0
 			i = 0
@@ -326,12 +470,8 @@ def draw(argv, doc_path):
 		h += 0.25
 		if h > 1:
 			h -= 1
-		#v = (1 - v)
-		#v *= 0.8
-		#s *= 1.1
 		s = 1 - s
 		v = 1 - v
-		#s = max(s, 1)
 		return f"hsv({h*360}, {s*100}%, {v*100}%)"
 
 	deffont = ImageFont.truetype("basicbit3.ttf", 28)
@@ -344,8 +484,6 @@ def draw(argv, doc_path):
 		for i in range(hours_diff):
 			draw.rectangle((get_x(((low // (60 * 60))+i)*60*60),0,get_x(((low//(60*60))+i)*60*60)+1,height), fill="#000000")
 
-	# draw.text()
-
 	for include in included_entries:
 		# print("Drawing", include, get_x(include["xml"].attrib["begin"]), get_x(include["xml"].attrib["end"])-get_x(include["xml"].attrib["begin"]))
 		color = palette[include["xml"].tag] if include["xml"].tag in palette else "#ffffff"
@@ -354,12 +492,12 @@ def draw(argv, doc_path):
 		level = include["level"]*120
 		
 		if end < begin:
-			log(f"Can't draw element <{include['xml'].tag} id={include['xml'].attrib['id']}> because begin is {dt.datetime.fromtimestamp(begin).strftime('%m/%d/%y %I:%M %p')} while end is {dt.datetime.fromtimestamp(end).strftime('%m/%d/%y %I:%M %p')}")
+			log(f"Can't draw element <{include['xml'].tag} id={include['xml'].attrib['id']}> because begin is {dt.datetime.fromtimestamp(begin).strftime('%m/%d/%y %H:%M')} while end is {dt.datetime.fromtimestamp(end).strftime('%m/%d/%y %H:%M')}")
 			continue
 
-		string_a = dt.datetime.fromtimestamp(begin).strftime("%I:%M %p")
+		string_a = dt.datetime.fromtimestamp(begin).strftime("%H:%M")
 		string_b = include["xml"].tag
-		string_c = dt.datetime.fromtimestamp(end).strftime("%I:%M %p")
+		string_c = dt.datetime.fromtimestamp(end).strftime("%H:%M")
 
 		draw.rectangle((get_x(begin), level, get_x(end), level+120), fill=color)
 		
@@ -372,4 +510,4 @@ def draw(argv, doc_path):
 	image.save("time.png")
 
 if __name__ == "__main__":
-	ztime_main(sys.argv, "time.xml")
+	ztime_main(sys.argv, "/mnt/Code/c++/ztime/time.xml")
